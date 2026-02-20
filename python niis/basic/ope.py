d7ef46d2a5ebbe1befefad952f987bbfd271bcfa
Anita Maity <anitamaity760@gmail.com>
2026-02-20 18:00:28 +0530

    basic

---
+++ python niis/basic/ope.py
@@ -0,0 +1,10 @@
+a=5
+b=7
+c=3
+c=a*3
+b=a//4
+print(a,b,c)
+a=b>c
+c=5 or b
+b=c>3 and b<7
+print(a,b,c)
---
+++ python niis/or.py
@@ -0,0 +1,8 @@
+print(5 or 7)
+print("hi" or "bye")
+print("" or "bye")
+print(3.5 or 5)
+a=10
+b=20
+print(a>5 or b<12)
+print(a<5 or b<12)
