7f22544b9a2a96d3818dc6cf59bc835c7a0686e3
Anita Maity <anitamaity760@gmail.com>
2026-02-22 11:47:16 +0530

    pro15,16,17,18,19,20

---
+++ python niis/pro15,16,17,18,19,20/celsius.py
@@ -0,0 +1,5 @@
+16.# celsius to fahrenheit.
+print("enter celsius")
+C=float(input())
+F=(C*9/5)+32
+print("fahrenheit=",F)
---
+++ python niis/pro15,16,17,18,19,20/square.py
@@ -0,0 +1,5 @@
+15.# square root question.
+print("enter number")
+n=float(input())
+sqrt=n**0.5
+print("square root=",sqrt)
