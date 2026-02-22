2a2d15f01f161f3d67f5da6d10dcfc9f95644ded
Anita Maity <anitamaity760@gmail.com>
2026-02-22 11:18:45 +0530

    program10,11,12,13,14

---
+++ python niis/program10,11,12,13,14/power.py
@@ -0,0 +1,7 @@
+10.# power calculation question.
+print("enter base")
+b=int(input())
+print("enter exponent")
+e=int(input())
+res=b**e
+print("result=",res)
---
+++ python niis/program10,11,12,13,14/quotient.py
@@ -0,0 +1,9 @@
+11.# quotient and remainder.
+print("enter first number")
+a=int(input())
+print("enter second number")
+b=int(input())
+q=a//b
+r=a%b
+print("quotient=",q)
+print("remainder=",r)
