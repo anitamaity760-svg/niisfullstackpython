c63924d56bf4cf3988dab10fa1e468e5d4cbc692
Anita Maity <anitamaity760@gmail.com>
2026-04-22 11:04:40 +0530

    python niis

---
+++ python niis/djangoprogram/Empproject/emp_project/emp_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    salary = models.FloatField()
+
+    def __str__(self):
+        return self.namefrom django.db import models
+
+# Create your models here.
